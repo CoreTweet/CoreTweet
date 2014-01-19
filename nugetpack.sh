428012c05686c912c207dbbbdc5c79d07a48d5df
Alice <marisachar@gmail.com>
2014-01-20 02:54:21 +0900

    Publish to Nuget (2)

--- nugetpack.sh
+++ nugetpack.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nuget pack CoreTweet/CoreTweet.nuspec -OutputDirectory Binary/Nightly
-nuget pack CoreTweet.Streaming.Reactive/CoreTweet.Streaming.Reactive.nuspec -OutputDirectory Binary/Nightly
+nuget pack CoreTweet.nuspec -OutputDirectory Binary/Nightly
+nuget pack CoreTweet.Streaming.Reactive.nuspec -OutputDirectory Binary/Nightly
