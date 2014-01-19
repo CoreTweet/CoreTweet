ede7b78afff19f928e86e5175872f1b3ea17c1a2
Yuta Sato <cannotdebug@gmail.com>
2014-01-20 02:05:39 +0900

    Publish to Nuget
    and create some scripts for developing.

--- CoreTweet.Streaming.Reactive/Properties/AssemblyInfo.cs
+++ CoreTweet.Streaming.Reactive/Properties/AssemblyInfo.cs
@@ -18,6 +18,8 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("0.1.5")]
+[assembly: AssemblyFileVersion("0.1.5")]
+
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
