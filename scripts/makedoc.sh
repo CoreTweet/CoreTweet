c27410da66271761f95436f5600e1841f804f74e
Alice <cannotdebug@gmail.com>
2014-03-23 14:40:10 +0900

    Update makedoc.sh

--- scripts/makedoc.sh
+++ scripts/makedoc.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/zsh
-cd Binary/Nightly
+cd Binary/Nightly/net40
 rm -rf docs*
-mdoc update --delete CoreTweet.dll -i CoreTweet.xml CoreTweet.Streaming.Reactive.dll -i CoreTweet.Streaming.Reactive.xml -o ./docs-data/
+mdoc update --delete CoreTweet.dll -i CoreTweet.xml CoreTweet.Streaming.Reactive.dll -i CoreTweet.Streaming.Reactive.xml -o ../docs-data/
+cd ..
 mdoc export-html -o ./docs/ ./docs-data/
 cd docs
 sed -i -e "s/Documentation for this section has not yet been entered.//g" **/*.html -u
 sed -i -e "s/Untitled/CoreTweet/g" **/*.html -u
-sed -i -e "s/Alice.Extensions Namespace//g" **/*.html -u
 rm -rf Alice*
 
