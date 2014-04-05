1be9f2b5b6b59dd5baa772b0be7255d02b881cb0
Alice <cannotdebug@gmail.com>
2014-04-05 23:15:55 +0900

    Bump to v0.2.3

--- CoreTweet/AssemblyInfo.cs
+++ CoreTweet/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.2.2.*")]
-[assembly: AssemblyInformationalVersion("0.2.2")]
+[assembly: AssemblyVersion("0.2.3.*")]
+[assembly: AssemblyInformationalVersion("0.2.3")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("CoreTweetDebug")]
