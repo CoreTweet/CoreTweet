a0e648ea746e1f51bec96a87aee3229ab5ceb377
Alice <cannotdebug@gmail.com>
2014-04-08 16:40:28 +0900

    Bump to 0.2.4

--- CoreTweet.Streaming.Reactive/AssemblyInfo.cs
+++ CoreTweet.Streaming.Reactive/AssemblyInfo.cs
@@ -16,8 +16,8 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.2.3.*")]
-[assembly: AssemblyInformationalVersion("0.2.3")]
+[assembly: AssemblyVersion("0.2.4.*")]
+[assembly: AssemblyInformationalVersion("0.2.4")]
 
 
 // The following attributes are used to specify the signing key for the assembly, 
