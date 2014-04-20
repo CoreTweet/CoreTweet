104036364d1b7371b1598e4183f91046a30dc182
Alice <cannotdebug@gmail.com>
2014-04-20 21:45:54 +0900

    Change nuget repo to mono/nuget-binary
    libcurl problem with Codeplex

--- scripts/make_external.sh
+++ scripts/make_external.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 git submodule update --init --recursive
 cd ExternalDependencies/doxygen
-./configure
-make
-cd ../nuget
-./build.sh
+./configure || {
+    echo 'error: while runnning configure' ;
+    exit 1; 
+}
+make || {
+    echo 'error: while runnning make' ;
+    exit 1; 
+}
--- scripts/nuget_restore.sh
+++ scripts/nuget_restore.sh
@@ -2,6 +2,6 @@
 for i in CoreTweet*/
 do
   cd $i
-  mono ../ExternalDependencies/nuget/src/CommandLine/bin/Release/NuGet.exe restore -ConfigFile packages.config -PackagesDirectory ../packages
+  mono ../ExternalDependencies/nuget/NuGet.exe restore -ConfigFile packages.config -PackagesDirectory ../packages
   cd ..
 done
