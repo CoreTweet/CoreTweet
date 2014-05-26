CoreTweet/CoreTweet

commit 5021ebeb7c903dc2eee5057c7f29bad7b334394d

Use HttpClient in Win8.1/WP8.1/Win8

CoreTweet/Internal/TokensBase.Async.cs
@@ -55,7 +55,7 @@ namespace CoreTweet.Core
                 .ContinueWith(
                     t => InternalUtils.ReadResponse(t, s => CoreBase.Convert<T>(this, s, jsonPath), cancellationToken),
                     cancellationToken
-                ).CheckCanceled(cancellationToken);
+                ).Unwrap().CheckCanceled(cancellationToken);
         }
 
         internal Task<ListedResponse<T>> AccessApiArrayAsync<T>(MethodType type, string url, Expression<Func<string, object>>[] parameters, string jsonPath = "")
@@ -79,7 +79,7 @@ namespace CoreTweet.Core
                 .ContinueWith(
                     t => InternalUtils.ReadResponse(t, s => new ListedResponse<T>(CoreBase.ConvertArray<T>(this, s, jsonPath)), cancellationToken),
                     cancellationToken
-                ).CheckCanceled(cancellationToken);
+                ).Unwrap().CheckCanceled(cancellationToken);
         }
 
         internal Task AccessApiNoResponseAsync(string url, Expression<Func<string, object>>[] parameters)
@@ -104,11 +104,8 @@ namespace CoreTweet.Core
                 {
                     if(t.IsFaulted)
                         t.Exception.Handle(ex => false);
-#if PCL || WIN_RT
+
                     t.Result.Dispose();
-#else
-                    t.Result.Close();
-#endif
                 }, cancellationToken);
         }
 
@@ -130,7 +127,7 @@ namespace CoreTweet.Core
         /// <param name='parameters'>
         /// Parameters.
         /// </param>
-        public Task<HttpWebResponse> SendRequestAsync(MethodType type, string url, CancellationToken cancellationToken = default(CancellationToken), params Expression<Func<string, object>>[] parameters)
+        public Task<AsyncResponse> SendRequestAsync(MethodType type, string url, CancellationToken cancellationToken = default(CancellationToken), params Expression<Func<string, object>>[] parameters)
         {
             return this.SendRequestAsyncImpl(type, url, InternalUtils.ExpressionsToDictionary(parameters), this.ConnectionOptions, cancellationToken);
         }
@@ -153,7 +150,7 @@ namespace CoreTweet.Core
         /// <param name="cancellationToken">
         /// Cancellation token.
         /// </param>
-        public Task<HttpWebResponse> SendRequestAsync<T>(MethodType type, string url, T parameters, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<AsyncResponse> SendRequestAsync<T>(MethodType type, string url, T parameters, CancellationToken cancellationToken = default(CancellationToken))
         {
             return this.SendRequestAsyncImpl(type, url, InternalUtils.ResolveObject(parameters), this.ConnectionOptions, cancellationToken);
         }
@@ -176,12 +173,12 @@ namespace CoreTweet.Core
         /// <param name="cancellationToken">
         /// Cancellation token.
         /// </param>
-        public Task<HttpWebResponse> SendRequestAsync(MethodType type, string url, IDictionary<string, object> parameters, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<AsyncResponse> SendRequestAsync(MethodType type, string url, IDictionary<string, object> parameters, CancellationToken cancellationToken = default(CancellationToken))
         {
             return this.SendRequestAsyncImpl(type, url, parameters, this.ConnectionOptions, cancellationToken);
         }
 
-        public Task<HttpWebResponse> SendStreamingRequestAsync(MethodType type, string url, IEnumerable<KeyValuePair<string, object>> parameters, CancellationToken cancellationToken = default(CancellationToken))
+        public Task<AsyncResponse> SendStreamingRequestAsync(MethodType type, string url, IEnumerable<KeyValuePair<string, object>> parameters, CancellationToken cancellationToken = default(CancellationToken))
         {
             var options = this.ConnectionOptions != null ? (ConnectionOptions)this.ConnectionOptions.Clone() : new ConnectionOptions();
 #if !(PCL || WIN_RT || WP)
@@ -190,27 +187,46 @@ namespace CoreTweet.Core
             return this.SendRequestAsyncImpl(type, url, parameters, options, cancellationToken);
         }
 
-        private static HttpWebResponse ResponseCallback(Task<HttpWebResponse> t)
+        private static
+#if WIN_RT
+        async
+#endif
+        Task<AsyncResponse> ResponseCallback(Task<AsyncResponse> t)
         {
+#if WIN_RT
             if(t.IsFaulted)
+                throw t.Exception.InnerException;
+
+            if(!t.Result.Source.IsSuccessStatusCode)
             {
-                t.Exception.Handle(ex =>
+                var tex = await TwitterException.Create(t.Result.Source);
+                if(tex != null)
+                    throw tex;
+                t.Result.Source.EnsureSuccessStatusCode();
+            }
+
+            return t.Result;
+#else
+            return Task.Factory.StartNew(() =>
+            {
+                if(t.IsFaulted)
                 {
-                    var wex = ex as WebException;
-                    if(ex != null)
+                    var wex = t.Exception.InnerException as WebException;
+                    if(wex != null)
                     {
                         var tex = TwitterException.Create(wex);
                         if(tex != null)
                             throw tex;
                     }
-                    return false;
-                });
-            }
+                    throw t.Exception.InnerException;
+                }
 
-            return t.Result;
+                return t.Result;
+            });
+#endif
         }
 
-        private Task<HttpWebResponse> SendRequestAsyncImpl(MethodType type, string url, IEnumerable<KeyValuePair<string, object>> parameters, ConnectionOptions options, CancellationToken cancellationToken)
+        private Task<AsyncResponse> SendRequestAsyncImpl(MethodType type, string url, IEnumerable<KeyValuePair<string, object>> parameters, ConnectionOptions options, CancellationToken cancellationToken)
         {
             var prmArray = CollectionToCommaSeparatedString(parameters);
             if(type != MethodType.Get && prmArray.Any(x => x.Value is Stream || x.Value is IEnumerable<byte>
@@ -225,7 +241,9 @@ namespace CoreTweet.Core
                     CreateAuthorizationHeader(type, url, null),
                     options,
                     cancellationToken
-                ).ContinueWith(new Func<Task<HttpWebResponse>, HttpWebResponse>(ResponseCallback), cancellationToken);
+                )
+                .ContinueWith(new Func<Task<AsyncResponse>, Task<AsyncResponse>>(ResponseCallback), cancellationToken)
+                .Unwrap();
             }
             else
             {
@@ -245,7 +263,9 @@ namespace CoreTweet.Core
                         options,
                         cancellationToken
                     )
-                ).ContinueWith(new Func<Task<HttpWebResponse>, HttpWebResponse>(ResponseCallback), cancellationToken);
+                )
+                .ContinueWith(new Func<Task<AsyncResponse>, Task<AsyncResponse>>(ResponseCallback), cancellationToken)
+                .Unwrap();
             }
         }
     }

CoreTweet/Request.Async.cs
@@ -23,13 +23,109 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+#if WIN8
+using System.Net.Http;
+#elif WIN_RT
+using Windows.Web.Http;
+#endif
+
+#if WIN_RT
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Storage;
+using Windows.Storage.Streams;
+#endif
+
 namespace CoreTweet
 {
+    public class AsyncResponse : IDisposable
+    {
+#if WIN_RT
+        public AsyncResponse(HttpResponseMessage source)
+#elif PCL
+        internal AsyncResponse(HttpWebResponse source)
+#else
+        public AsyncResponse(HttpWebResponse source)
+#endif
+        {
+            this.Source = source;
+            this.StatusCode = (int)source.StatusCode;
+#if WIN8
+            this.Headers = source.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.FirstOrDefault());
+#elif WIN_RT
+            this.Headers = source.Headers;
+#else
+            this.Headers = source.Headers.AllKeys.ToDictionary(k => k, k => source.Headers[k]);
+#endif
+        }
+
+#if WIN_RT
+        public HttpResponseMessage Source { get; private set; }
+#elif PCL
+        private HttpWebResponse Source { get; set; }
+#else
+        public HttpWebResponse Source { get; private set; }
+#endif
+
+        public int StatusCode { get; private set; }
+
+        public IDictionary<string, string> Headers { get; private set; }
+
+        public Stream GetResponseStream()
+        {
+#if WIN_RT
+            throw new PlatformNotSupportedException();
+#else
+            return this.Source.GetResponseStream();
+#endif
+        }
+
+#if WIN_RT && !WIN8
+        public async Task<Stream> GetResponseStreamAsync()
+        {
+            return (await this.Source.Content.ReadAsInputStreamAsync()).AsStreamForRead(0);
+        }
+#else
+        public Task<Stream> GetResponseStreamAsync()
+        {
+#if WIN8
+            return this.Source.Content.ReadAsStreamAsync();
+#else
+            return Task.Factory.StartNew(() => this.Source.GetResponseStream());
+#endif
+        }
+#endif
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                if(this.Source != null)
+                {
+#if PCL || WIN_RT
+                    this.Source.Dispose();
+#else
+                    this.Source.Close();
+#endif
+                }
+                this.Source = null;
+                this.Headers = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
     partial class Request
     {
         private static void DelayAction(int timeout, CancellationToken cancellationToken, Action action)
@@ -46,6 +142,38 @@ namespace CoreTweet
 #endif
         }
 
+#if WIN_RT
+        private static Task<AsyncResponse> ExecuteRequest(HttpClient client, HttpRequestMessage req, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
+        {
+            req.Headers.Add("Authorization", authorizationHeader);
+            req.Headers.Add("User-Agent", options.UserAgent);
+#if WIN8
+            req.Headers.ExpectContinue = false;
+#endif
+            if(options.BeforeRequestAction != null)
+                options.BeforeRequestAction(req);
+            var cancellation = new CancellationTokenSource();
+            var reg = cancellationToken.Register(cancellation.Cancel);
+#if WIN8
+            var task = client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead, cancellation.Token);
+#else
+            var task = client.SendRequestAsync(req, HttpCompletionOption.ResponseHeadersRead).AsTask(cancellation.Token);
+#endif
+            var timeoutCancellation = new CancellationTokenSource();
+            DelayAction(options.Timeout, timeoutCancellation.Token, cancellation.Cancel);
+            return task.ContinueWith(t =>
+            {
+                timeoutCancellation.Cancel();
+                reg.Dispose();
+                if(t.IsFaulted)
+                    throw t.Exception.InnerException;
+                if(!cancellationToken.IsCancellationRequested && cancellation.IsCancellationRequested)
+                    throw new TimeoutException();
+                return new AsyncResponse(t.Result);
+            }, cancellationToken);
+        }
+#endif
+
         /// <summary>
         /// Sends a GET request.
         /// </summary>
@@ -56,9 +184,19 @@ namespace CoreTweet
         /// <param name="userAgent">User-Agent header.</param>
         /// <param name="proxy">Proxy information for the request.</param>
         /// <param name="cancellationToken">Cancellation token.</param>
-        internal static Task<HttpWebResponse> HttpGetAsync(string url, IEnumerable<KeyValuePair<string, object>> prm, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
+        internal static Task<AsyncResponse> HttpGetAsync(string url, IEnumerable<KeyValuePair<string, object>> prm, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
         {
-            var task = new TaskCompletionSource<HttpWebResponse>();
+            if(options == null) options = new ConnectionOptions();
+            if(prm == null) prm = new Dictionary<string, object>();
+            var reqUrl = url + '?' + CreateQueryString(prm);
+
+#if WIN_RT
+            var client = new HttpClient();
+            var req = new HttpRequestMessage(HttpMethod.Get, new Uri(reqUrl));
+            req.Headers.Add("Authorization", authorizationHeader);
+            return ExecuteRequest(client, req, authorizationHeader, options, cancellationToken);
+#else
+            var task = new TaskCompletionSource<AsyncResponse>();
             if(cancellationToken.IsCancellationRequested)
             {
                 task.TrySetCanceled();
@@ -68,8 +206,7 @@ namespace CoreTweet
             try
             {
                 if(prm == null) prm = new Dictionary<string, object>();
-                if(options == null) options = new ConnectionOptions();
-                var req = (HttpWebRequest)WebRequest.Create(url + '?' + CreateQueryString(prm));
+                var req = (HttpWebRequest)WebRequest.Create(reqUrl);
 
                 var reg = cancellationToken.Register(() =>
                 {
@@ -77,25 +214,27 @@ namespace CoreTweet
                     req.Abort();
                 });
 
-#if NET45 || WIN_RT || WP
+#if NET45 || WP
                 req.AllowReadStreamBuffering = false;
 #endif
-#if !(PCL || WIN_RT || WP)                
+#if !(PCL || WP)                
                 req.ReadWriteTimeout = options.ReadWriteTimeout;
 #endif
-#if !(PCL || WIN_RT)
+#if !PCL
                 req.UserAgent = options.UserAgent;
 #endif
 #if !(PCL || WP)
                 req.Proxy = options.Proxy;
 #endif
                 req.Headers[HttpRequestHeader.Authorization] = authorizationHeader;
+#if !PCL
                 if(options.BeforeRequestAction != null) options.BeforeRequestAction(req);
+#endif
 
                 var timeoutCancellation = new CancellationTokenSource();
                 DelayAction(options.Timeout, timeoutCancellation.Token, () =>
                 {
-#if !(PCL || WIN_RT) //If PCL, Abort will throw RequestCanceled
+#if !PCL //If PCL, Abort will throw RequestCanceled
                     task.TrySetException(new WebException("Timeout", WebExceptionStatus.Timeout));
 #endif
                     req.Abort();
@@ -106,7 +245,7 @@ namespace CoreTweet
                     reg.Dispose();
                     try
                     {
-                        task.TrySetResult((HttpWebResponse)req.EndGetResponse(ar));
+                        task.TrySetResult(new AsyncResponse((HttpWebResponse)req.EndGetResponse(ar)));
                     }
                     catch(Exception ex)
                     {
@@ -120,6 +259,7 @@ namespace CoreTweet
             }
 
             return task.Task;
+#endif
         }
 
         /// <summary>
@@ -132,9 +272,25 @@ namespace CoreTweet
         /// <param name="userAgent">User-Agent header.</param>
         /// <param name="proxy">Proxy information for the request.</param>
         /// <param name="cancellationToken">Cancellation token.</param>
-        internal static Task<HttpWebResponse> HttpPostAsync(string url, IEnumerable<KeyValuePair<string, object>> prm, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
+        internal static Task<AsyncResponse> HttpPostAsync(string url, IEnumerable<KeyValuePair<string, object>> prm, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
         {
-            var task = new TaskCompletionSource<HttpWebResponse>();
+            if(options == null) options = new ConnectionOptions();
+            if(prm == null) prm = new Dictionary<string, object>();
+
+#if WIN_RT
+            var client = new HttpClient();
+            var req = new HttpRequestMessage(HttpMethod.Post, new Uri(url));
+            req.Content =
+#if WIN8
+                new FormUrlEncodedContent(
+#else
+                new HttpFormUrlEncodedContent(
+#endif
+                    prm.Select(kvp =>new KeyValuePair<string, string>(kvp.Key, kvp.Value.ToString()))
+                );
+            return ExecuteRequest(client, req, authorizationHeader, options, cancellationToken);
+#else
+            var task = new TaskCompletionSource<AsyncResponse>();
             if(cancellationToken.IsCancellationRequested)
             {
                 task.TrySetCanceled();
@@ -143,8 +299,6 @@ namespace CoreTweet
 
             try
             {
-                if(options == null) options = new ConnectionOptions();
-                if(prm == null) prm = new Dictionary<string, object>();
                 var data = Encoding.UTF8.GetBytes(CreateQueryString(prm));
                 var req = (HttpWebRequest)WebRequest.Create(url);
 
@@ -154,29 +308,31 @@ namespace CoreTweet
                     req.Abort();
                 });
 
-#if NET45 || WIN_RT || WP
+#if NET45|| WP
                 req.AllowReadStreamBuffering = false;
 #endif
                 req.Method = "POST";
                 req.ContentType = "application/x-www-form-urlencoded";
                 req.Headers[HttpRequestHeader.Authorization] = authorizationHeader;
-#if !(PCL || WIN_RT || WP)
+#if !(PCL || WP)
                 req.ServicePoint.Expect100Continue = false;
                 req.ReadWriteTimeout = options.ReadWriteTimeout;
 #endif
-#if !(PCL || WIN_RT)
+#if !PCL
                 req.UserAgent = options.UserAgent;
                 req.ContentLength = data.Length;
 #endif
 #if !(PCL || WP)
                 req.Proxy = options.Proxy;
 #endif
+#if !PCL
                 if(options.BeforeRequestAction != null) options.BeforeRequestAction(req);
+#endif
 
                 var timeoutCancellation = new CancellationTokenSource();
                 DelayAction(options.Timeout, timeoutCancellation.Token, () =>
                 {
-#if !(PCL || WIN_RT)
+#if !PCL
                     task.TrySetException(new WebException("Timeout", WebExceptionStatus.Timeout));
 #endif
                     req.Abort();
@@ -194,7 +350,7 @@ namespace CoreTweet
                             reg.Dispose();
                             try
                             {
-                                task.TrySetResult((HttpWebResponse)req.EndGetResponse(resAr));
+                                task.TrySetResult(new AsyncResponse((HttpWebResponse)req.EndGetResponse(resAr)));
                             }
                             catch(Exception ex)
                             {
@@ -214,6 +370,7 @@ namespace CoreTweet
             }
 
             return task.Task;
+#endif
         }
 
         /// <summary>
@@ -226,9 +383,74 @@ namespace CoreTweet
         /// <param name="userAgent">User-Agent header.</param>
         /// <param name="proxy">Proxy information for the request.</param>
         /// <param name="cancellationToken">Cancellation token.</param>
-        internal static Task<HttpWebResponse> HttpPostWithMultipartFormDataAsync(string url, IEnumerable<KeyValuePair<string, object>> prm, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
+        internal static
+#if WIN_RT
+        async
+#endif
+        Task<AsyncResponse> HttpPostWithMultipartFormDataAsync(string url, IEnumerable<KeyValuePair<string, object>> prm, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
         {
-            var task = new TaskCompletionSource<HttpWebResponse>();
+            if(options == null) options = new ConnectionOptions();
+
+#if WIN_RT
+            var client = new HttpClient();
+            var req = new HttpRequestMessage(HttpMethod.Post, new Uri(url));
+#if WIN8
+            var content = new MultipartFormDataContent();
+#else
+            var content = new HttpMultipartFormDataContent();
+#endif
+            foreach(var x in prm)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var valueStream = x.Value as Stream;
+                var valueInputStream = x.Value as IInputStream;
+                var valueBytes = x.Value as IEnumerable<byte>;
+                var valueBuffer = x.Value as IBuffer;
+                var valueInputStreamReference = x.Value as IInputStreamReference;
+                var valueStorageItem = x.Value as IStorageItem;
+
+                if(valueInputStreamReference != null)
+                    valueInputStream = await valueInputStreamReference.OpenSequentialReadAsync();
+
+#if WIN8
+                if(valueInputStream != null)
+                    valueStream = valueInputStream.AsStreamForRead();
+                if(valueBuffer != null)
+                    valueStream = valueBuffer.AsStream();
+
+                if(valueStream != null)
+                    content.Add(new StreamContent(valueStream), x.Key, valueStorageItem != null ? valueStorageItem.Name : "file");
+                else if(valueBytes != null)
+                {
+                    var valueByteArray = valueBytes as byte[];
+                    if(valueByteArray == null) valueByteArray = valueBytes.ToArray();
+                    content.Add(new ByteArrayContent(valueByteArray), x.Key, valueStorageItem != null ? valueStorageItem.Name : "file");
+                }
+                else
+                    content.Add(new StringContent(x.Value.ToString()), x.Key);
+#else
+                if (valueStream != null)
+                    valueInputStream = valueStream.AsInputStream();
+                if (valueBytes != null)
+                {
+                    var valueByteArray = valueBytes as byte[];
+                    if (valueByteArray == null) valueByteArray = valueBytes.ToArray();
+                    valueBuffer = valueByteArray.AsBuffer();
+                }
+
+                if (valueInputStream != null)
+                    content.Add(new HttpStreamContent(valueInputStream), x.Key, valueStorageItem != null ? valueStorageItem.Name : "file");
+                else if (valueBuffer != null)
+                    content.Add(new HttpBufferContent(valueBuffer), x.Key, valueStorageItem != null ? valueStorageItem.Name : "file");
+                else
+                    content.Add(new HttpStringContent(x.Value.ToString()), x.Key);
+#endif
+            }
+            cancellationToken.ThrowIfCancellationRequested();
+            return await ExecuteRequest(client, req, authorizationHeader, options, cancellationToken);
+#else
+            var task = new TaskCompletionSource<AsyncResponse>();
             if(cancellationToken.IsCancellationRequested)
             {
                 task.TrySetCanceled();
@@ -237,7 +459,6 @@ namespace CoreTweet
 
             try
             {
-                if(options == null) options = new ConnectionOptions();
                 var boundary = Guid.NewGuid().ToString();
                 var req = (HttpWebRequest)WebRequest.Create(url);
 
@@ -247,15 +468,15 @@ namespace CoreTweet
                     req.Abort();
                 });
 
-#if NET45 || WIN_RT || WP
+#if NET45 || WP
                 req.AllowReadStreamBuffering = false;
 #endif
                 req.Method = "POST";
-#if !(PCL || WIN_RT || WP)
+#if !(PCL || WP)
                 req.ServicePoint.Expect100Continue = false;
                 req.ReadWriteTimeout = options.ReadWriteTimeout;
 #endif
-#if !(PCL || WIN_RT)
+#if !PCL
                 req.UserAgent = options.UserAgent;
 #endif
 #if !(PCL || WP)
@@ -263,12 +484,14 @@ namespace CoreTweet
 #endif
                 req.ContentType = "multipart/form-data;boundary=" + boundary;
                 req.Headers[HttpRequestHeader.Authorization] = authorizationHeader;
+#if !PCL
                 if(options.BeforeRequestAction != null) options.BeforeRequestAction(req);
+#endif
 
                 var timeoutCancellation = new CancellationTokenSource();
                 DelayAction(options.Timeout, timeoutCancellation.Token, () =>
                 {
-#if !(PCL || WIN_RT)
+#if !PCL
                     task.TrySetException(new WebException("Timeout", WebExceptionStatus.Timeout));
 #endif
                     req.Abort();
@@ -286,7 +509,7 @@ namespace CoreTweet
                             reg.Dispose();
                             try
                             {
-                                task.TrySetResult((HttpWebResponse)req.EndGetResponse(resAr));
+                                task.TrySetResult(new AsyncResponse((HttpWebResponse)req.EndGetResponse(resAr)));
                             }
                             catch(Exception ex)
                             {
@@ -306,6 +529,7 @@ namespace CoreTweet
             }
 
             return task.Task;
+#endif
         }
     }
 }

commit a3be0a146fc05c889efc928d2864d0ce4a4a36fb

Fix bugs caused by 5021ebeb7c903dc2eee5057c7f29bad7b334394d

CoreTweet.Streaming.Reactive/Stream.cs
@@ -58,9 +58,17 @@ namespace CoreTweet.Streaming.Reactive
                     .ContinueWith(task =>
                     {
                         if(task.IsFaulted)
-                            task.Exception.Handle(ex => false);
+                            throw task.Exception.InnerException;
 
-                        using(var reader = new StreamReader(task.Result.GetResponseStream()))
+                        return task.Result.GetResponseStreamAsync();
+                    }, cancel)
+                    .Unwrap()
+                    .ContinueWith(task =>
+                    {
+                        if(task.IsFaulted)
+                            throw task.Exception.InnerException;
+
+                        using(var reader = new StreamReader(task.Result))
                         using(var reg = cancel.Register(() => reader.Dispose()))
                         {
                             foreach(var s in reader.EnumerateLines().Where(x => !string.IsNullOrEmpty(x)))
@@ -73,7 +81,7 @@ namespace CoreTweet.Streaming.Reactive
                     .ContinueWith(task =>
                     {
                         if(task.IsFaulted)
-                            task.Exception.Handle(ex => false);
+                            throw task.Exception.InnerException;
                     }, cancel);
             });
         }

CoreTweet/Internal/TokensBase.Async.cs
@@ -30,6 +30,11 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
+#if WIN_RT
+using Windows.Storage;
+using Windows.Storage.Streams;
+#endif
+
 namespace CoreTweet.Core
 {
     partial class TokensBase
@@ -232,6 +237,9 @@ namespace CoreTweet.Core
             if(type != MethodType.Get && prmArray.Any(x => x.Value is Stream || x.Value is IEnumerable<byte>
 #if !(PCL || WIN_RT)
                 || x.Value is FileInfo
+#endif
+#if WIN_RT
+                || x.Value is IInputStream || x.Value is IBuffer || x.Value is IInputStreamReference || x.Value is IStorageItem
 #endif
                ))
             {

CoreTweet/Request.Async.cs
@@ -32,8 +32,10 @@ using System.Threading.Tasks;
 
 #if WIN8
 using System.Net.Http;
+using System.Net.Http.Headers;
 #elif WIN_RT
 using Windows.Web.Http;
+using Windows.Web.Http.Headers;
 #endif
 
 #if WIN_RT
@@ -145,10 +147,14 @@ namespace CoreTweet
 #if WIN_RT
         private static Task<AsyncResponse> ExecuteRequest(HttpClient client, HttpRequestMessage req, string authorizationHeader, ConnectionOptions options, CancellationToken cancellationToken)
         {
-            req.Headers.Add("Authorization", authorizationHeader);
+            var splitAuth = authorizationHeader.Split(new[] { ' ' }, 2);
             req.Headers.Add("User-Agent", options.UserAgent);
 #if WIN8
             req.Headers.ExpectContinue = false;
+            req.Headers.Authorization = new AuthenticationHeaderValue(splitAuth[0], splitAuth[1]);
+#else
+            req.Headers.Expect.Clear();
+            req.Headers.Authorization = new HttpCredentialsHeaderValue(splitAuth[0], splitAuth[1]);
 #endif
             if(options.BeforeRequestAction != null)
                 options.BeforeRequestAction(req);
@@ -448,6 +454,7 @@ namespace CoreTweet
 #endif
             }
             cancellationToken.ThrowIfCancellationRequested();
+            req.Content = content;
             return await ExecuteRequest(client, req, authorizationHeader, options, cancellationToken);
 #else
             var task = new TaskCompletionSource<AsyncResponse>();