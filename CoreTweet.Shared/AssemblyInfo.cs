3296c841b06b1bff19d7ddd96ec259915ff73215
Alice <cannotdebug@gmail.com>
2015-01-28 18:55:32 +0900

    Bump to 0.4.1

--- CoreTweet.Shared/AssemblyInfo.cs
+++ CoreTweet.Shared/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.4.0.*")]
-[assembly: AssemblyInformationalVersion("0.4.0")]
+[assembly: AssemblyVersion("0.4.1.*")]
+[assembly: AssemblyInformationalVersion("0.4.1")]
 
 
 [assembly: InternalsVisibleTo("CoreTweet.Streaming.Reactive")]
