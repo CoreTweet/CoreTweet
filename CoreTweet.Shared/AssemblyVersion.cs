c37565e579aa14815725754f226f3370f7dc2e5f
azyobuzin <azyobuzin@users.sourceforge.jp>
2015-02-15 20:16:36 +0900

    AssemblyVersion.cs

---
+++ CoreTweet.Shared/AssemblyVersion.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.4.1.*")]
+[assembly: AssemblyInformationalVersion("0.4.1")]
