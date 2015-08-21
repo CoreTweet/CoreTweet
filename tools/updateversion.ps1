3c605e3131a866d266776621c794d626fe19ab4e
azyobuzin <azyobuzin@users.sourceforge.jp>
2015-08-21 21:58:44 +0900

    Bump to 0.5.3

---
+++ tools/updateversion.ps1
@@ -0,0 +1,17 @@
+﻿param (
+    [string]$oldver,
+    [string]$newver
+)
+
+$yml = ls "*.yml"
+$nuspec = ls "*.nuspec"
+$foofile = ls "*file"
+$cs = ls -Path ".\CoreTweet.Shared" -Filter "*.cs" -Recurse
+
+foreach ($x in $yml + $nuspec + $foofile + $cs) {
+    $before = Get-Content $x.FullName -Raw
+    $after = $before -replace $oldver, $newver
+    if ($before -ne $after) {
+        [IO.File]::WriteAllText($x.FullName, $after)
+    }
+}
