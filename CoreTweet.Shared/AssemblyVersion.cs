e161d26323586ae21cee3dec73ae6ef3b8af33a2
azyobuzin <azyobuzin@users.sourceforge.jp>
2016-01-18 09:46:43 +0900

    Bump to 0.6.1

--- CoreTweet.Shared/AssemblyVersion.cs
+++ CoreTweet.Shared/AssemblyVersion.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.6.0.*")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyVersion("0.6.1.*")]
+[assembly: AssemblyInformationalVersion("0.6.1")]
