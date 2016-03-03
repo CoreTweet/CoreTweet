aa93cf1fb9747105ce67dd75f3b9cc0c5d8befc2
azyobuzin <azyobuzin@users.sourceforge.jp>
2016-03-04 00:24:33 +0900

    Refactor WriteMultipartFormData

--- CoreTweet.Shared/Request.cs
+++ CoreTweet.Shared/Request.cs
@@ -68,176 +68,51 @@ namespace CoreTweet
 #endif
         )
         {
-            const int bufferSize = 81920;
+#if !(NET35 || NET40 || PCL)
+            long bytesSent = 0;
+            long? totalBytesToSend = 0;
+#endif
+
+            Action<int> reportProgress =
+#if (NET35 || NET40 || PCL)
+                null;
+#else
+                progress == null ? (Action<int>)null
+                    : bytes =>
+                    {
+                        bytesSent += bytes;
+                        progress.Report(new UploadProgressInfo(bytesSent, totalBytesToSend));
+                    };
+#endif
+
+            var items = prm.ConvertAll(x => MultipartItem.Create(x.Key, x.Value, reportProgress));
 
 #if !(NET35 || NET40 || PCL)
             // Compute total bytes
-            long bytesSent = 0;
-            long? totalBytesToSend = 0;
-            Action<int> reportProgress = bytes =>
-            {
-                if (progress != null)
-                {
-                    bytesSent += bytes;
-                    progress.Report(new UploadProgressInfo(bytesSent, totalBytesToSend));
-                }
-            };
             if (progress != null)
             {
-                foreach(var x in prm)
+                foreach (var x in items)
                 {
-                    var valueStream = x.Value as Stream;
-                    if(valueStream != null)
+                    var f = x as FileMultipartItem;
+                    if (f != null)
                     {
-                        try
-                        {
-                            totalBytesToSend += valueStream.Length;
-                            continue;
-                        }
-                        catch
+                        var len = f.Length;
+                        if (len == null)
                         {
                             totalBytesToSend = null;
                             break;
                         }
-                    }
-
-                    var valueArraySegment = x.Value as ArraySegment<byte>?;
-                    if(valueArraySegment != null)
-                    {
-                        totalBytesToSend += valueArraySegment.Value.Count;
-                        continue;
-                    }
-
-                    var valueByteArray = x.Value as byte[];
-                    if(valueByteArray != null)
-                    {
-                        totalBytesToSend += valueByteArray.Length;
-                        continue;
-                    }
-                   
-
-                    var valueFile = x.Value as FileInfo;
-                    if(valueFile != null)
-                    {
-                        totalBytesToSend += valueFile.Length;
-                        continue;
-                    }
-
-                    if(x.Value is IEnumerable<byte>
-#if WP
-                        || x.Value is Windows.Storage.Streams.IInputStream
-#endif
-                    )
-                    {
-                        totalBytesToSend = null;
-                        break;
+                        totalBytesToSend += len;
                     }
                 }
-                progress.Report(new UploadProgressInfo(0, totalBytesToSend));
             }
 #endif
 
             // Start writing
-            foreach(var x in prm)
+            foreach(var x in items)
             {
-                var valueStream = x.Value as Stream;
-                var valueArraySegment = x.Value as ArraySegment<byte>?;
-                var valueBytes = x.Value as IEnumerable<byte>;
-#if !PCL
-                var valueFile = x.Value as FileInfo;
-#endif
-                var valueString = x.Value.ToString();
-#if WP
-                var valueInputStream = x.Value as Windows.Storage.Streams.IInputStream;
-                if(valueInputStream != null) valueStream = valueInputStream.AsStreamForRead();
-#endif
-
                 stream.WriteString("--" + boundary + "\r\n");
-                if(valueStream != null || valueBytes != null || valueArraySegment != null
-#if !PCL
-                    || valueFile != null
-#endif
-                   )
-                {
-                    stream.WriteString("Content-Type: application/octet-stream\r\n");
-                }
-                stream.WriteString(string.Format(@"Content-Disposition: form-data; name=""{0}""", x.Key));
-#if !PCL
-                if(valueFile != null)
-                    stream.WriteString(string.Format(@"; filename=""{0}""",
-                        valueFile.Name.Replace("\n", "%0A").Replace("\r", "%0D").Replace("\"", "%22")));
-                else
-#endif
-                if(valueStream != null || valueBytes != null || valueArraySegment != null)
-                    stream.WriteString(@"; filename=""file""");
-                stream.WriteString("\r\n\r\n");
-
-#if !PCL
-                if(valueFile != null)
-                    valueStream = valueFile.OpenRead();
-#endif
-                if(valueStream != null)
-                {
-                    var buffer = new byte[bufferSize];
-                    int count;
-                    while((count = valueStream.Read(buffer, 0, bufferSize)) > 0)
-                    {
-                        stream.Write(buffer, 0, count);
-#if !(NET35 || NET40 || PCL)
-                        reportProgress(count);
-#endif
-                    }
-                }
-                else if(valueArraySegment != null)
-                {
-                    stream.Write(valueArraySegment.Value.Array, valueArraySegment.Value.Offset, valueArraySegment.Value.Count);
-#if !(NET35 || NET40 || PCL)
-                    reportProgress(valueArraySegment.Value.Count);
-#endif
-                }
-                else if(valueBytes != null)
-                {
-                    var buffer = valueBytes as byte[];
-                    if(buffer != null)
-                    {
-                        stream.Write(buffer, 0, buffer.Length);
-#if !(NET35 || NET40 || PCL)
-                        reportProgress(buffer.Length);
-#endif
-                    }
-                    else
-                    {
-                        buffer = new byte[bufferSize];
-                        var i = 0;
-                        foreach(var b in valueBytes)
-                        {
-                            buffer[i++] = b;
-                            if(i == bufferSize)
-                            {
-                                stream.Write(buffer, 0, bufferSize);
-#if !(NET35 || NET40 || PCL)
-                                reportProgress(bufferSize);
-#endif
-                                i = 0;
-                            }
-                        }
-                        if(i > 0)
-                        {
-                            stream.Write(buffer, 0, i);
-#if !(NET35 || NET40 || PCL)
-                            reportProgress(i);
-#endif
-                        }
-                    }
-                }
-                else
-                    stream.WriteString(valueString);
-
-#if !PCL
-                if(valueFile != null)
-                    valueStream.Close();
-#endif
-
+                x.WriteTo(stream);
                 stream.WriteString("\r\n");
             }
             stream.WriteString("--" + boundary + "--");
