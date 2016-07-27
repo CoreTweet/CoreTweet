df6c26bb99e6e15de9f9fb9078453f82310310bd
azyobuzin <azyobuzin@users.sourceforge.jp>
2016-07-28 02:25:56 +0900

    Set AssemblyFileVersion
    to prevent project.json from overwriting AssemblyFileVersion

--- CoreTweet.Shared/AssemblyVersion.cs
+++ CoreTweet.Shared/AssemblyVersion.cs
@@ -5,4 +5,5 @@ using System.Reflection;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 [assembly: AssemblyVersion("0.7.0.*")]
+[assembly: AssemblyFileVersion("0.7.0")]
 [assembly: AssemblyInformationalVersion("0.7.0")]
