cc2631b1916831c49c41370cf189bad08a27ea92
cannorin <cannorin@users.noreply.github.com>
2018-09-09 15:18:34 +0900

    [WIP] add CRC response generation helper

---
+++ CoreTweet.Shared/AccountActivity/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CoreTweet.Core;
+using System.Text;
+namespace CoreTweet.AccountActivity
+{
+    public static class WebhookUtils
+    {
+        public static string GenerateCrcJsonResponse(this TokensBase tokens, string crcToken)
+        {
+            var res = SecurityUtils.HmacSha256(Encoding.UTF8.GetBytes(tokens.ConsumerSecret), Encoding.UTF8.GetBytes(crcToken));
+            return $"{{\"response_token\": \"sha256={res}\"}}";
+        }
+    }
+}
