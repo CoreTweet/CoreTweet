7d6b64d6cbd7a822d710104a9675afd5cbf2e32a
cannorin <cannorin@users.noreply.github.com>
2018-09-09 18:18:37 +0900

    base64

--- CoreTweet.Shared/AccountActivity/Utils.cs
+++ CoreTweet.Shared/AccountActivity/Utils.cs
@@ -8,7 +8,7 @@ namespace CoreTweet.AccountActivity
         public static string GenerateCrcJsonResponse(this TokensBase tokens, string crcToken)
         {
             var res = SecurityUtils.HmacSha256(Encoding.UTF8.GetBytes(tokens.ConsumerSecret), Encoding.UTF8.GetBytes(crcToken));
-            return $"{{\"response_token\": \"sha256={res}\"}}";
+            return $"{{\"response_token\": \"sha256={Convert.ToBase64String(res)}\"}}";
         }
     }
 }
